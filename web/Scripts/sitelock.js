6c22c9d605d06859bf094868b61d27e533e82a6d
Fernando Noyola <fernandonoyola211@gmail.com>
2024-01-16 20:59:47 +0000

    Update sitelock.js

--- web/Scripts/sitelock.js
+++ web/Scripts/sitelock.js
@@ -13,6 +13,6 @@
 //⠀⠀⠙⢶⣤⣀⣀⣠⣴⡀⢸⣿⠇⠀⢀⣖⣦⣤⣤⠀⠀⠈⣿⣄⠀⠀⠀⣿⡆⠀⠀⢠⣿⡇⠀⠀⢸⣿⡇⠀⠀⠈⠉⠁⠀⠀⠀⣿⣶⣤⣀⣀⣀⣠⣤⣾⡿⠏⠀
 //⠀⠀⠀⠀⠉⠙⠛⠛⠋⠛⠿⠛⢶⣶⣾⡿⠁⠀⠈⢦⣤⣤⣼⣿⣄⣀⣠⣿⠹⣄⣀⣾⡿⢧⣀⣀⣼⣿⠰⣤⣤⣤⣴⣶⣶⣶⣿⡿⠃⠉⠙⠛⠛⠛⠋⠁
 
-//if (window.self === window.top) {
- //   window.location.href = 'https://www.google.com/';
-//}
+if (window.self === window.top) {
+    window.location.href = 'https://www.google.com/';
+}
