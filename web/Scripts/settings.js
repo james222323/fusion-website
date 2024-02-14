6681795ee450413e2e9e4c0651170449d9b35354
Fernando Noyola <fernandonoyola211@gmail.com>
2024-02-14 15:05:56 +0000

    Update settings.js

--- web/Scripts/settings.js
+++ web/Scripts/settings.js
@@ -19,7 +19,7 @@ setAccentStrength()
 
 function setAccentStrength(){
     let accent = localStorage.getItem("accentColor");
-    if(!accent) accent = "None (Default)";
+    if(!accent) accent = "Strong 💪";
     let strength = localStorage.getItem("accentStrength");
     if(!strength) localStorage.setItem("accentStrength", "Good");
 
